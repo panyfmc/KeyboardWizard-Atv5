8d190694192e72e439fe46091e8f253dac92bcae
GabrielDosSantosAlmeida <bieljvsa@gmail.com>
2024-06-29 15:01:16 -0300

    feat: adiciona tela de registro não funcional

---
+++ Conta/TelaDeRegistro/registro.js
@@ -0,0 +1,11 @@
+function onChangeEmail(){
+
+}
+
+
+const form = {
+    email: () => document.getElementById("email"),
+    ConfirmEmail: () => document.getElementById("confirmEmail"),
+    password: () => document.getElementById("password"),
+    ConfirmPassword: () => document.getElementById("confirmPassword")
+}
