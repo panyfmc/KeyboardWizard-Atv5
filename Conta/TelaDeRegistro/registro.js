bda277989e5f3ced8ce6e4f08e374813897e48c9
GabrielDosSantosAlmeida <bieljvsa@gmail.com>
2024-06-30 11:48:01 -0300

    feat: vincula o registro com o banco de dados FireBase

--- Conta/TelaDeRegistro/registro.js
+++ Conta/TelaDeRegistro/registro.js
@@ -6,6 +6,7 @@ function onChangeEmail(){
     form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
     // Chama a validação de confirmação de email
     validateEmailMatch();
+    toggleRegisterButtonDisable();
 }
 
 //função que valida Senha
@@ -24,11 +25,13 @@ function onChangePassword(){
     form.passwordCaracteresError().style.display = isPasswordInvalid ? "block" : "none";
     //função que válida password sendo chamada
     validatePasswordMatch();
+    toggleRegisterButtonDisable();
 }
 
 //função para chamar a validação da password
 function OnChangeConfirmPassword(){
     validatePasswordMatch();
+    toggleRegisterButtonDisable();
 }
 
 //função que valida senha
@@ -40,8 +43,48 @@ function validatePasswordMatch(){
     password == confirmPassword ? "none" : "block";
 }
 
+//função para reativar o botão
+function toggleRegisterButtonDisable() {
+    form.registerButton().disabled = !isFormValid();
+}
+//validando todo formulario para ativar botão
+function isFormValid() {
+    //valida email
+    const email = form.email().value;
+    if (!email || !validateEmail(email)) {
+        return false;
+    }
+    //valida confirmação de email
+    const confirmEmail = form.ConfirmEmail().value;
+    if(email != confirmEmail){
+        return false;
+    }
+    //valida senha
+    const password = form.password().value;
+    const isValidLength = password.length >= 8; //maior ou igual que 8 caracteres
+    const hasLowercase = /[a-z]/.test(password); //conter uma letra minúscula
+    const hasNumber = /[0-9]/.test(password); // conter um número
+    const hasUppercase = /[A-Z]/.test(password); // conter uma letra maiúscula
+    const hasSpecialChar = /[!@#$%*()_+^&{}}:;?.]/.test(password); //conter um caractere especial
+
+    // Se qualquer pré-requisito não for atendido, exibir mensagem de erro
+    const isPasswordInvalid = !isValidLength || !hasLowercase || !hasNumber || !hasUppercase || !hasSpecialChar;
+    if (!password || isPasswordInvalid) {
+        return false;
+    }
+    //valida confirmação de senha
+    const confirmPassword = form.ConfirmPassword().value;
+    if (password != confirmPassword) {
+        return false;
+    }
+    //se não, retorna true
+    return true;
+}
+
+//função para chamar a validação de email
 function onChangeConfirmEmail() {
     validateEmailMatch();
+    toggleRegisterButtonDisable();
 }
 
 // Função que valida a confirmação de email
@@ -49,11 +92,36 @@ function validateEmailMatch() {
     const email = form.email().value;
     const confirmEmail = form.ConfirmEmail().value;
 
-
     form.ConfirmEmailDoesntMatchError().style.display = 
     email == confirmEmail ? "none" : "block";
 }
+//função que coloca os dados do registro no firebase
+function register(){
+    showLoading();//animação carregar
 
+    const email = form.email().value;
+    const password = form.password().value;
+    firebase.auth().createUserWithEmailAndPassword(
+        email, password
+    ).then(() => {
+        hideLoading();//tirando animação
+        window.location.href = "/Conta/TelaDePerfil/TelaDePerfil.html";
+    }).catch(error =>{
+        hideLoading();//tirando animação
+        alert(getErrorMessageRegist(error));
+    })
+}
+//função que redireciona pra página de Login
+function gologin(){
+    window.location.href = "/Conta/TelaDeLogin/TelaDeLogin.html";
+}
+//função para exibir error(usuário já cadastrado)
+function getErrorMessageRegist(error){
+    if(error.code == "auth/email-already-in-use"){
+        return "Email já está em uso"
+    }
+    return error.message;
+}
 //formulario com todos os dados que tem input
 const form = {
     email: () => document.getElementById("email"),
@@ -66,4 +134,5 @@ const form = {
     ConfirmPasswordDoesntMatchError: () => document.getElementById("password-doesnt-match-error"),
     passwordRequiredError: () => document.getElementById("password-required-error"),
     passwordCaracteresError: () => document.getElementById("password-min-length-error"),
+    registerButton: () => document.getElementById('register-button')
 }
---
+++ Conta/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDWFq9N3ZQj8KdHuprU3GZKPBU0Vy-W9Iw",
+    authDomain: "keyboardwizard-54df8.firebaseapp.com",
+    projectId: "keyboardwizard-54df8",
+    storageBucket: "keyboardwizard-54df8.appspot.com",
+    messagingSenderId: "31513420231",
+    appId: "1:31513420231:web:951745db1edd6afc0939e2"
+};
+firebase.initializeApp(firebaseConfig);  
